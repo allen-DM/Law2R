463a913fafe5b6dd67b3a24335088a34e33949b2
DESKTOP-8U7E8U4\allen <h88129@gmail.com>
2021-04-21 22:34:35 +0800

    fix

--- R/cleanLawRawdata.R
+++ R/cleanLawRawdata.R
@@ -11,25 +11,27 @@
 
 #' @rdname cleanLawRawdata
 #' @export
-cleanLawRawdata <- function(...){
+cleanLawRawdata <- function(law_Rawdata,...){
   UseMethod("cleanLawRawdata")
 }
 
+##(公務員懲戒委員會) ----
 #' @rdname cleanLawRawdata
 #' @export
-cleanLawRawdata.disciplinaryCommittee <- function(...){
+cleanLawRawdata.disciplinaryCommittee <- function(law_Rawdata,...){
   tryCatch({
+    gsub_nr <-
+      law_Rawdata$jfull %>%  gsub("[\n]", "", .) %>%  gsub("[\r]", "", .) %>%  gsub(" ", "", .)
+    #法院
+    area <- strsplit(gsub_nr,"　　　　　　　　　　　　　")[[1]][[1]]
     # 擷取相關人員 ----
-    #(公務員懲戒委員會) ----
-    #str_locate(char.vec, pattern = "ti") 找特定字串在資料中的位置(第一個)
-    #str_locate_all(char.vec, pattern = "ti") 找特定字串在資料中的全部位置
-    index1 <- str_locate(gsub_nr, pattern = "主席委員")
+    index1 <- stringr::str_locate(gsub_nr, pattern = "主席委員")
+    peoplelist <-stringr::str_sub(gsub_nr, start = index1[1])
 
-    peoplelist <-str_sub(gsub_nr, start = index1[1])
+    #彙整相關人員
+    peoplelist_index <- stringr::str_locate_all(peoplelist, pattern = "委員")
+    endpoint_index <- stringr::str_locate(peoplelist, pattern = "右正本證明")
 
-    peoplelist_index <- str_locate_all(peoplelist, pattern = "委員")
-
-    endpoint_index <- str_locate(peoplelist, pattern = "右正本證明")
     #擷取委員長及各委員
     committeeMember= c()
     for (i in 1:nrow(peoplelist_index[[1]])) { #i=1
@@ -39,8 +41,10 @@ cleanLawRawdata.disciplinaryCommittee <- function(...){
     }
     #擷取書記官
     clerk_index <- str_locate(gsub_nr, pattern = "書記官")
+    #人員
     Clerk <- str_sub(gsub_nr, start = clerk_index[2]+1)
-    #彙整相關人員
+    mainjudge <- paste0(jsonlite::toJSON(list("委員長"=str_sub(committeeMember[1], start =2)),auto_unbox = T))
+    secondaryjudge <- paste0(jsonlite::toJSON(list("委員"=committeeMember[-1]),auto_unbox = T))
 
     #擷取結論
     conclusion_index <- str_locate(gsub_nr, pattern = "據上論結")
@@ -50,7 +54,17 @@ cleanLawRawdata.disciplinaryCommittee <- function(...){
         start = str_locate(gsub_nr, pattern = "據上論結")[2] + 2 ,
         end = str_locate(gsub_nr, pattern = "如主文。")[2]
       )
-    return(conclusion)
+    tmplist <- list(
+      law_Rawdata[1,]$judgmentid,
+      law_Rawdata[1,]$jdate,
+      mainjudge,
+      secondaryjudge,
+      Clerk,
+      conclusion,
+      area
+    )
+    utility$sqlDbSendStatement("INSERT INTO `legal`.`legalinfo` (`judgmentid`, `datetime`, `mainjudge`, `secondaryjudge`, `clerk`, `conclusion`, `class`) VALUES (?, ?, ?, ?, ?, ?, ?);",tmplist)
+    return("新增成功")
   },error = function(msg) {
     message("utility.rawdata Original error message:")
     message(paste0(msg,"\n"))
--- R/utility.R
+++ R/utility.R
@@ -20,13 +20,32 @@ utility <- list(
       con <- utility$dbGetconncet()
       result <- DBI::dbGetQuery(con, query)
       DBI::dbDisconnect(con)
+      return(result)
     },# 遇到 error 時的自訂處理函數
     error = function(msg) {
       DBI::dbDisconnect(con)
       message("utility.dbQuery error message:")
       message(paste0(msg,"\n"))
-      return(NA)
+      return("fail")
     })
-    return(result)
+  },
+  sqlDbSendStatement = function(sql,tmplist){
+    tryCatch({
+      con <- utility$dbGetconncet()
+      rs <- DBI::dbSendStatement(
+        con,
+        sql
+      )
+      DBI::dbBind(rs, tmplist)
+      DBI::dbClearResult(rs)
+      DBI::dbDisconnect(con)
+      return("OK")
+    },# 遇到 error 時的自訂處理函數
+    error = function(msg) {
+      DBI::dbDisconnect(con)
+      message("utility.dbWriteTable error message:")
+      message(paste0(msg,"\n"))
+      return("fail")
+    })
   }
 )
