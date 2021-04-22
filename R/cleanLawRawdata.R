751e5872e8e37e22a0d6e020fa23b9fd5843ef59
DESKTOP-8U7E8U4\allen <h88129@gmail.com>
2021-04-22 23:59:37 +0800

    first data clean complie

--- R/cleanLawRawdata.R
+++ R/cleanLawRawdata.R
@@ -1,73 +1,237 @@
-#' cleanLawRawdata
-#'
-#' A function to clean law raw data into lawinfo
-#' @param x law data
-#' @keyword lawinfo
-#' @importFrom stringr dplyr
-#' @export cleanLawRawdata
-#' @examples
-#' mean(1:3)
-#' \dontrun{ mean(1:1e99) }
 
-#' @rdname cleanLawRawdata
-#' @export
-cleanLawRawdata <- function(law_Rawdata,...){
-  UseMethod("cleanLawRawdata")
-}
+cleanLawRawdata<- list(
+    ##(公務員懲戒委員會) ----
+    disciplinaryCommittee = function(law_Rawdata,...){
+      tryCatch({
+        gsub_nr <-
+          law_Rawdata$jfull %>%  gsub("[\n]", "", .) %>%  gsub("[\r]", "", .) %>%  gsub(" ", "", .)
+        #類別
+        class <- strsplit(gsub_nr,"　")[[1]][[1]]
+        # 擷取相關人員 ----
+        index1 <- stringr::str_locate(gsub_nr, pattern = "主席委員")
+        peoplelist <-stringr::str_sub(gsub_nr, start = index1[1])
 
-##(公務員懲戒委員會) ----
-#' @rdname cleanLawRawdata
-#' @export
-cleanLawRawdata.disciplinaryCommittee <- function(law_Rawdata,...){
-  tryCatch({
-    gsub_nr <-
-      law_Rawdata$jfull %>%  gsub("[\n]", "", .) %>%  gsub("[\r]", "", .) %>%  gsub(" ", "", .)
-    #法院
-    area <- strsplit(gsub_nr,"　　　　　　　　　　　　　")[[1]][[1]]
-    # 擷取相關人員 ----
-    index1 <- stringr::str_locate(gsub_nr, pattern = "主席委員")
-    peoplelist <-stringr::str_sub(gsub_nr, start = index1[1])
+        #彙整相關人員
+        peoplelist_index <- stringr::str_locate_all(peoplelist, pattern = "委員")
+        endpoint_index <- stringr::str_locate(peoplelist, pattern = "右正本證明")
 
-    #彙整相關人員
-    peoplelist_index <- stringr::str_locate_all(peoplelist, pattern = "委員")
-    endpoint_index <- stringr::str_locate(peoplelist, pattern = "右正本證明")
+        #擷取委員長及各委員
+        committeeMember= c()
+        for (i in 1:nrow(peoplelist_index[[1]])) { #i=1
+          start_index = peoplelist_index[[1]][i,][2]+1
+          if(i == nrow(peoplelist_index[[1]])){end_index <- endpoint_index[1,][1]-1}else{end_index = peoplelist_index[[1]][i+1,][1]-1}
+          committeeMember[i] <- str_sub(peoplelist, start = start_index, end = end_index)
+        }
+        #擷取書記官
+        clerk_index <- str_locate(gsub_nr, pattern = "書記官")
+        #人員
+        Clerk <- str_sub(gsub_nr, start = clerk_index[2]+1)
+        if(nchar(Clerk)>6){ Clerk <- "未記載"}
+        mainjudge <- paste0(jsonlite::toJSON(list("委員長"=str_sub(committeeMember[1], start =2)),auto_unbox = T))
+        secondaryjudge <- paste0(jsonlite::toJSON(list("委員"=committeeMember[-1]),auto_unbox = T))
 
-    #擷取委員長及各委員
-    committeeMember= c()
-    for (i in 1:nrow(peoplelist_index[[1]])) { #i=1
-      start_index = peoplelist_index[[1]][i,][2]+1
-      if(i == nrow(peoplelist_index[[1]])){end_index <- endpoint_index[1,][1]-1}else{end_index = peoplelist_index[[1]][i+1,][1]-1}
-      committeeMember[i] <- str_sub(peoplelist, start = start_index, end = end_index)
+        #擷取結論
+        conclusion_index <- str_locate(gsub_nr, pattern = "據上論結")
+        conclusion <-
+          str_sub(
+            gsub_nr,
+            start = str_locate(gsub_nr, pattern = "據上論結")[2] + 2 ,
+            end = str_locate(gsub_nr, pattern = "如主文。")[2]
+          )
+        tmplist <- list(
+          law_Rawdata[1,]$judgmentid,
+          law_Rawdata[1,]$jdate,
+          mainjudge,
+          secondaryjudge,
+          Clerk,
+          conclusion,
+          class
+        )
+        utility$sqlDbSendStatement("INSERT INTO `legal`.`legalinfo` (`judgmentid`, `datetime`, `mainjudge`, `secondaryjudge`, `clerk`, `conclusion`, `class`) VALUES (?, ?, ?, ?, ?, ?, ?);",tmplist)
+        return(paste0("id = ",tmplist[1]," 新增成功"))
+      },error = function(msg) {
+        message("utility.rawdata Original error message:")
+        return(paste0("id = ",tmplist[1]," 新增失敗","\n",msg))
+      })
+
+    },
+    ##(司法院冤獄賠償覆議委員會決定書) ----
+
+    injusticeCompensation=function(law_Rawdata){
+      tryCatch({
+        gsub_nr <-
+          law_Rawdata$jfull %>%  gsub("[\n]", "", .) %>%  gsub("[\r]", "", .) %>%  gsub(" ", "", .)
+
+        #類別
+        class <- strsplit(gsub_nr,"　")[[1]][[1]]
+
+        # 擷取相關人員 ----
+        index1 <- stringr::str_locate(gsub_nr, pattern = "主席委員")
+        peoplelist <-stringr::str_sub(gsub_nr, start = index1[1])
+
+        #彙整相關人員
+        peoplelist_index <- stringr::str_locate_all(peoplelist, pattern = "委員")
+        endpoint_index <- stringr::str_locate(peoplelist, pattern = "右正本證明")
+
+        #擷取委員長及各委員
+        committeeMember= c()
+        for (i in 1:nrow(peoplelist_index[[1]])) { #i=1
+          start_index = peoplelist_index[[1]][i,][2]+1
+          if(i == nrow(peoplelist_index[[1]])){end_index <- endpoint_index[1,][1]-1}else{end_index = peoplelist_index[[1]][i+1,][1]-1}
+          committeeMember[i] <- str_sub(peoplelist, start = start_index, end = end_index)
+        }
+
+        #擷取書記官
+        clerk_index <- str_locate(gsub_nr, pattern = "書記官")
+
+        #人員
+        Clerk <- str_sub(gsub_nr, start = clerk_index[2]+1)
+        if(nchar(Clerk)>6){ Clerk <- "未記載"}
+        mainjudge <- paste0(jsonlite::toJSON(list("委員長"=str_sub(committeeMember[1], start =2)),auto_unbox = T))
+        secondaryjudge <- paste0(jsonlite::toJSON(list("委員"=committeeMember[-1]),auto_unbox = T))
+
+        #擷取結論
+        conclusion_index <- str_locate(gsub_nr, pattern = "據上論結")
+        conclusion <-
+          str_sub(
+            gsub_nr,
+            start = str_locate(gsub_nr, pattern = "據上論結")[2] + 2 ,
+            end = str_locate(gsub_nr, pattern = "如主文。")[2]
+          )
+        tmplist <- list(
+          law_Rawdata[1,]$judgmentid,
+          law_Rawdata[1,]$jdate,
+          mainjudge,
+          secondaryjudge,
+          Clerk,
+          conclusion,
+          class
+        )
+        utility$sqlDbSendStatement("INSERT INTO `legal`.`legalinfo` (`judgmentid`, `datetime`, `mainjudge`, `secondaryjudge`, `clerk`, `conclusion`, `class`) VALUES (?, ?, ?, ?, ?, ?, ?);",tmplist)
+        return(paste0("id = ",tmplist[1]," 新增成功"))
+        },error = function(msg) {
+          message("utility.rawdata Original error message:")
+          message(paste0(msg,"\n"))
+          return(paste0("id = ",tmplist[1]," 新增失敗","\n",msg))
+        })
+      },
+    ##(最高法院民事裁定) ----
+    supremeCourtCivil=function(law_Rawdata){
+      tryCatch({
+        gsub_nr <-
+          law_Rawdata$jfull %>%  gsub("[\n]", "", .) %>%  gsub("[\r]", "", .) %>%  gsub(" ", "", .)
+
+        #類別
+        class <- strsplit(gsub_nr,"　")[[1]][[1]]
+
+        # 擷取相關人員 ----
+        index1 <- stringr::str_locate(gsub_nr, pattern = "審判長法官")
+        peoplelist <-stringr::str_sub(gsub_nr, start = index1[1])
+
+        #彙整相關人員
+        peoplelist_index <- stringr::str_locate_all(peoplelist, pattern = "法官")
+        endpoint_index <- stringr::str_locate(peoplelist, pattern = "右正本證明")
+
+        #擷取委員長及各委員
+        committeeMember= c()
+        for (i in 1:nrow(peoplelist_index[[1]])) { #i=1
+          start_index = peoplelist_index[[1]][i,][2]+1
+          if(i == nrow(peoplelist_index[[1]])){end_index <- endpoint_index[1,][1]-1}else{end_index = peoplelist_index[[1]][i+1,][1]-1}
+          committeeMember[i] <- str_sub(peoplelist, start = start_index, end = end_index)
+        }
+
+        #擷取書記官
+        clerk_index <- str_locate(gsub_nr, pattern = "書記官")
+
+        #人員
+        Clerk <- str_sub(gsub_nr, start = clerk_index[2]+1)
+        if(nchar(Clerk)>6){ Clerk <- "未記載"}
+        mainjudge <- paste0(jsonlite::toJSON(list("審判長法官"=str_sub(committeeMember[1], start =2)),auto_unbox = T))
+        secondaryjudge <- paste0(jsonlite::toJSON(list("法官"=committeeMember[-1]),auto_unbox = T))
+
+        #擷取結論
+        conclusion_index <- str_locate(gsub_nr, pattern = "據上論結")
+        conclusion <-
+          str_sub(
+            gsub_nr,
+            start = str_locate(gsub_nr, pattern = "據上論結")[2] + 2 ,
+            end = str_locate(gsub_nr, pattern = "如主文。")[2]
+          )
+        tmplist <- list(
+          law_Rawdata[1,]$judgmentid,
+          law_Rawdata[1,]$jdate,
+          mainjudge,
+          secondaryjudge,
+          Clerk,
+          conclusion,
+          class
+        )
+        utility$sqlDbSendStatement("INSERT INTO `legal`.`legalinfo` (`judgmentid`, `datetime`, `mainjudge`, `secondaryjudge`, `clerk`, `conclusion`, `class`) VALUES (?, ?, ?, ?, ?, ?, ?);",tmplist)
+        return(paste0("id = ",tmplist[1]," 新增成功"))
+      },error = function(msg) {
+        message("utility.rawdata Original error message:")
+        message(paste0(msg,"\n"))
+        return(paste0("id = ",tmplist[1]," 新增失敗","\n",msg))
+      })
+    },
+    ##(最高法院刑事判決) ----
+    supremeCourtCriminalJudgment=function(law_Rawdata){
+      tryCatch({
+        gsub_nr <-
+          law_Rawdata$jfull %>%  gsub("[\n]", "", .) %>%  gsub("[\r]", "", .) %>%  gsub(" ", "", .)
+
+        #類別
+        class <- strsplit(gsub_nr,"　")[[1]][[1]]
+
+        # 擷取相關人員 ----
+        index1 <- stringr::str_locate(gsub_nr, pattern = "審判長法官")
+        peoplelist <-stringr::str_sub(gsub_nr, start = index1[1])
+
+        #彙整相關人員
+        peoplelist_index <- stringr::str_locate_all(peoplelist, pattern = "法官")
+        endpoint_index <- stringr::str_locate(peoplelist, pattern = "右正本證明")
+
+        #擷取委員長及各委員
+        committeeMember= c()
+        for (i in 1:nrow(peoplelist_index[[1]])) { #i=1
+          start_index = peoplelist_index[[1]][i,][2]+1
+          if(i == nrow(peoplelist_index[[1]])){end_index <- endpoint_index[1,][1]-1}else{end_index = peoplelist_index[[1]][i+1,][1]-1}
+          committeeMember[i] <- str_sub(peoplelist, start = start_index, end = end_index)
+        }
+
+        #擷取書記官
+        clerk_index <- str_locate(gsub_nr, pattern = "書記官")
+
+        #人員
+        Clerk <- str_sub(gsub_nr, start = clerk_index[2]+1)
+        if(nchar(Clerk)>6){ Clerk <- "未記載"}
+        mainjudge <- paste0(jsonlite::toJSON(list("審判長法官"=str_sub(committeeMember[1], start =2)),auto_unbox = T))
+        secondaryjudge <- paste0(jsonlite::toJSON(list("法官"=committeeMember[-1]),auto_unbox = T))
+
+        #擷取結論
+        conclusion_index <- str_locate(gsub_nr, pattern = "據上論結")
+        conclusion <-
+          str_sub(
+            gsub_nr,
+            start = str_locate(gsub_nr, pattern = "據上論結")[2] + 2 ,
+            end = str_locate(gsub_nr, pattern = "如主文。")[2]
+          )
+        tmplist <- list(
+          law_Rawdata[1,]$judgmentid,
+          law_Rawdata[1,]$jdate,
+          mainjudge,
+          secondaryjudge,
+          Clerk,
+          conclusion,
+          class
+        )
+        utility$sqlDbSendStatement("INSERT INTO `legal`.`legalinfo` (`judgmentid`, `datetime`, `mainjudge`, `secondaryjudge`, `clerk`, `conclusion`, `class`) VALUES (?, ?, ?, ?, ?, ?, ?);",tmplist)
+        return(paste0("id = ",tmplist[1]," 新增成功"))
+      },error = function(msg) {
+        message("utility.rawdata Original error message:")
+        message(paste0(msg,"\n"))
+        return(paste0("id = ",tmplist[1]," 新增失敗","\n",msg))
+      })
     }
-    #擷取書記官
-    clerk_index <- str_locate(gsub_nr, pattern = "書記官")
-    #人員
-    Clerk <- str_sub(gsub_nr, start = clerk_index[2]+1)
-    mainjudge <- paste0(jsonlite::toJSON(list("委員長"=str_sub(committeeMember[1], start =2)),auto_unbox = T))
-    secondaryjudge <- paste0(jsonlite::toJSON(list("委員"=committeeMember[-1]),auto_unbox = T))
 
-    #擷取結論
-    conclusion_index <- str_locate(gsub_nr, pattern = "據上論結")
-    conclusion <-
-      str_sub(
-        gsub_nr,
-        start = str_locate(gsub_nr, pattern = "據上論結")[2] + 2 ,
-        end = str_locate(gsub_nr, pattern = "如主文。")[2]
-      )
-    tmplist <- list(
-      law_Rawdata[1,]$judgmentid,
-      law_Rawdata[1,]$jdate,
-      mainjudge,
-      secondaryjudge,
-      Clerk,
-      conclusion,
-      area
-    )
-    utility$sqlDbSendStatement("INSERT INTO `legal`.`legalinfo` (`judgmentid`, `datetime`, `mainjudge`, `secondaryjudge`, `clerk`, `conclusion`, `class`) VALUES (?, ?, ?, ?, ?, ?, ?);",tmplist)
-    return("新增成功")
-  },error = function(msg) {
-    message("utility.rawdata Original error message:")
-    message(paste0(msg,"\n"))
-    return(NA)
-  })
-}
+)
---
+++ R/controllerCleanRawLaw.R
@@ -0,0 +1,36 @@
+#### 原始資料整理
+controllerCleanRawLaw <- function(tmpText){
+
+  findClass=function (tmp){
+    gsub_nr <-
+      tmp$jfull %>%  gsub("[\n]", "", .) %>%  gsub("[\r]", "", .) %>%  gsub(" ", "", .) %>% strsplit(.,"　")
+    gsub_nr <- gsub_nr[[1]][[1]]
+    return(gsub_nr)
+  }
+
+  tryCatch({
+    if(findClass(tmpText)=="公務員懲戒委員會議決書"){
+      result=cleanLawRawdata$disciplinaryCommittee(tmpText)
+      return(paste0("公務員懲戒委員會議決書 add new data: ",result))
+
+    }else if(findClass(tmpText)=="司法院冤獄賠償覆議委員會決定書"){
+      result=cleanLawRawdata$injusticeCompensation(tmpText)
+      return(paste0("司法院冤獄賠償覆議委員會決定書 add new data: ",result))
+
+    }else if(findClass(tmpText)=="最高法院民事裁定" || findClass(tmpText)=="最高法院民事判決"){
+      result=cleanLawRawdata$supremeCourtCivil(tmpText)
+      return(paste0("最高法院民事裁定 add new data: ",result))
+    }else if(findClass(tmpText)=="最高法院民事判決"){
+      result=cleanLawRawdata$supremeCourtCivil(tmpText)
+      return(paste0("最高法院刑事判決 add new data: ",result))
+
+    }else{
+      return(paste0(tmpText$judgmentid,"cant be defind"))
+    }
+  },# 遇到 error 時的自訂處理函數
+  error = function(msg) {
+    message("utility.rawdata Original error message:")
+    message(paste0(msg,"\n"))
+    return(paste0("controllerCleanRawLaw add new data fail: ",msg))
+  })
+}
