0818942f504f53a5f68ddb294ff408145e894430
WHQ\10807061 <h88129@gmail.com>
2021-04-23 17:41:12 +0800

    add check function

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,11 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+checkLibrary <- function() {
+  libraryList=c("DBI","RMariaDB","jsonlite","httr","config","quantmod","dplyr","stringr","jiebaR")
+  for(i in 1:length(libraryList[i])){
+    if(require(libraryList[i])==FALSE){
+      install.packages(libraryList[i],dependencies=T)
+    }
+  }
 }
